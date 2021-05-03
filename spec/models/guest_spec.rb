5cef5daa1f4bdf43c956ef5725734a5ea0095574
mfcrespo <mafe_crespo@hotmail.com>
2021-05-03 10:35:38 -0500

    Add guest model

---
+++ spec/models/guest_spec.rb
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 Maria Crespo
+# Don't count the days, make the days count...Go Head!
+# All Rights Reserved
+
+require 'rails_helper'
+
+RSpec.describe Guest, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
